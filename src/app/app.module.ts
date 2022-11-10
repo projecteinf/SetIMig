3695332d6e7e1c2eac78317234fe5b5a760ac54b
projecteinf <projecteinf@boscdelacoma.cat>
2022-11-10 13:16:08 +0000

    Afegit component visualització carta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BarallaComponent } from './Projecte/Components/baralla/baralla.component';
+import { CartaComponent } from './Projecte/Components/carta/carta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarallaComponent
+    BarallaComponent,
+    CartaComponent
   ],
   imports: [
     BrowserModule
