b6776943e2b39335f45eaec1398ebc28fee7badf
projecteinf <projecteinf@boscdelacoma.cat>
2022-11-10 13:19:11 +0000

    Modificat drac.png per foc.png a Utils

--- src/app/Projecte/Utils/pals.ts
+++ src/app/Projecte/Utils/pals.ts
@@ -2,4 +2,4 @@ export const PAL = [
     {"pal":"PORRO","imatge":"porro.png"},
     {"pal":"FALÇ","imatge":"falç.png"},
     {"pal":"ROVELLO","imatge":"rovello.png"},
-    {"pal":"DRAC","imatge":"drac.png"}];
+    {"pal":"DRAC","imatge":"foc.png"}];
