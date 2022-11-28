b5330b90828a994ba28f32d4b2d77f041a433442
projecteinf <96139692+projecteinf@users.noreply.github.com>
2022-11-28 23:14:15 +0100

    Add files via upload

--- src/app/Projecte/Components/carta/carta.component.ts
+++ src/app/Projecte/Components/carta/carta.component.ts
@@ -1,28 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CartaSetIMig } from '../../Model/Entitats/Implementations/Carta/CartaSetIMig';
+import { APP_BASE_HREF } from '@angular/common';
+import { Component, Inject, inject, Input, OnInit } from '@angular/core';
+import { CartaSetIMig } from '../../Model/Entitats/Implementations/Carta/CartaSetIMig'; 
 import { BarallaService } from '../../Model/Services/baralla/baralla.service';
-import { JugadorService } from '../../Model/Services/jugadors/jugador.service';
-
+import { JugadorService } from '../../Model/Services/jugador/jugador.service';
 @Component({
   selector: 'app-carta',
   templateUrl: './carta.component.html',
   styleUrls: ['./carta.component.css']
 })
 export class CartaComponent implements OnInit {
-
-  constructor(private jugadorService:JugadorService,private barallaService:BarallaService) { }
   @Input() carta!: CartaSetIMig;
-  
-  ngOnInit(): void {
+  ruta!:string;
+  constructor(
+    private barallaService: BarallaService,
+    private jugadorService: JugadorService,
+    @Inject(APP_BASE_HREF) private baseHref: string) { }
 
+  ngOnInit(): void {
+    if (this.carta!=undefined) console.log(this.carta);
+    this.ruta = this.baseHref;
+    console.log(`RUTA: ${this.ruta}`);
   }
 
   agafar(carta:CartaSetIMig) {
-    if (this.jugadorService.getJugador().contar()<=7.5) {
-      carta.girar();
-      this.jugadorService.getJugador().agafarCarta(carta);
+    if(this.jugadorService.getJugador().contar()<=7.5) {
+      carta.girar(); // gira la carta!
+      this.jugadorService.getJugador().agafar(carta);
       this.barallaService.getBaralla().eliminar(carta);
     }
+    
+
   }
+
 
 }
